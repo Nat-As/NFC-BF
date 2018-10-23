cb8c91564150f33b98f744852f2972b176044269
James Andrews <42288915+Nat-As@users.noreply.github.com>
2018-10-23 13:20:44 -0400

    fixed progress
    fixed status

--- NFC-BF.c
+++ NFC-BF.c
@@ -14,10 +14,12 @@ int main(int argc, char **argv)
 	
 	for(;;){
 		for(int i=1;i<281474976710655;i++){//from 0 to ffffffffffff
+			
 			printf("%012x\n",i);//hex len 12
+
 			i++;
 			fflush(stdout); //pin progress
-			printf("\bIn progress: %ld", i/281474976710655); //progress
+			printf("\bKeys remaining: %ld", 281474976710655/i); //progress
 			printf("\e[?25l"); //hide cursor
 			fputs("\033[A\033[1K",stdout);//go up a line and clear
 		}
