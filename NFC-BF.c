2bb7cc68622ff9796355d419ff54908bfe849343
James Andrews <42288915+Nat-As@users.noreply.github.com>
2018-10-23 13:13:17 -0400

    corrected allignment
    added progress but progress is broken.

--- NFC-BF.c
+++ NFC-BF.c
@@ -14,8 +14,12 @@ int main(int argc, char **argv)
 	
 	for(;;){
 		for(int i=1;i<281474976710655;i++){//from 0 to ffffffffffff
-			printf("0x%014x\n",i);//hex
+			printf("%012x\n",i);//hex len 12
 			i++;
+			fflush(stdout); //pin progress
+			printf("\bIn progress: %ld", i/281474976710655); //progress
+			printf("\e[?25l"); //hide cursor
+			fputs("\033[A\033[1K",stdout);//go up a line and clear
 		}
 	}
 	return 0;
