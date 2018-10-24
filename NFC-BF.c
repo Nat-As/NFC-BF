438d9675be9fb7b3ee8f854bb2a4517ad62a2159
James Andrews <42288915+Nat-As@users.noreply.github.com>
2018-10-23 22:27:03 -0400

    V2.0

--- NFC-BF.c
+++ NFC-BF.c
@@ -7,6 +7,7 @@
 */
 
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char **argv)
 {
@@ -19,9 +20,11 @@ int main(int argc, char **argv)
 
 			i++;
 			fflush(stdout); //pin progress
-			printf("\bKeys remaining: %ld", 281474976710655/i); //progress
+			printf("\bKeys remaining: %ld", 281474976710655-i); //progress
 			printf("\e[?25l"); //hide cursor
 			fputs("\033[A\033[1K",stdout);//go up a line and clear
+			//add differential to calculate estimated cracking time
+			
 		}
 	}
 	return 0;
